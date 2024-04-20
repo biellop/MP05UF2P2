0532749519d323498b48a9abc2b1b79a078ec5e9
biellop <lopezescutiabiel@elpuig.xeill.net>
2024-04-20 16:42:31 +0200

    prueba unitaria

--- src/test/java/ex2/HashTableTest.java
+++ src/test/java/ex2/HashTableTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 
-class HashTableTest { //hola
+class HashTableTest { //holaa
 
     @ParameterizedTest
     @CsvSource({"1, elemento1"})
